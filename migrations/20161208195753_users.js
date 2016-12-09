95b276cb904d1fc45a8997bc97a2ac4bff5cf777
Anna Mefford <annamefford@gmail.com>
2016-12-08 20:11:59 -0700

    added npm modules, routes, seed and igration file for users, test file, validations file with docs, and knex files

---
+++ migrations/20161208195753_users.js
@@ -0,0 +1,9 @@
+"use strict";
+//TODO create user table
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
---
+++ routes/users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const ev = require('express-validation');
+const validations = require('../validations/comments');
+const knex = require('../knex');
+const {decamelizeKeys, camelizeKeys} = require('humps');
+
+router.get('/users', (req, res, next) => {
+
+});
+
+router.get('/users/:id', (req, res, next) => {
+
+});
+
+router.post('/users', ev(validations.post), (req, res, next) => {
+
+});
+
+router.patch('/users', ev(validations.patch), (req, res, next) => {
+
+});
+
+router.delete('/users', ev(validations.delete), (req, res, next) => {
+
+});
+
+module.exports = router;
