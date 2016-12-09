8ea9a38519f35a3f0d52d21bd2c2a2b157fe0acb
Anna Mefford <annamefford@gmail.com>
2016-12-09 12:11:57 -0700

    working on hashed passwords

--- migrations/20161208195753_users.js
+++ migrations/20161208195753_users.js
@@ -1,9 +1,15 @@
 'use strict';
-//TODO create user table
-exports.up = function(knex, Promise) {
 
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('username').notNullable().unique();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.boolean('admin').defaultTo('false');
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('users');
 };
