2175c13060f63ef9777119f859022918d1f49ea2
Anna Mefford <annamefford@gmail.com>
2016-12-09 14:20:38 -0700

    comment migration and seed file

--- migrations/20161208202018_comments.js
+++ migrations/20161208202018_comments.js
@@ -1,9 +1,17 @@
 'use strict';
 //TODO create comments table
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('comments', function(table){
+    table.increments();
+    table.integer('user_id').references('users.id').notNullable()
+    table.integer('observation_id').references('observations.id').notNullable();
+    table.text('comment').notNullable().defaultTo('');
+    table.integer('stars').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('comments');
 
 };
--- seeds/comments.js
+++ seeds/comments.js
@@ -2,13 +2,20 @@
 //TODO seed comments
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('comments').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
+        knex('comments').insert({
+          id: 1,
+          user_id: 1,
+          observation_id:1,
+          comment: "Stars were beautiful - great evening",
+          stars: 5
+      })
+    ]);
+  })
+      .then(function(){
+ return knex.raw("SELECT setval('comments_id_seq', (SELECT MAX(id) FROM comments))");
     });
 };
