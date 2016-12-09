0566807a76766cb28c6a72d135e4da60d4883809
Brad Efting <bradefting@gmail.com>
2016-12-09 14:45:23 -0700

    observation seed and migration files data added

--- seeds/observations.js
+++ seeds/observations.js
@@ -2,13 +2,42 @@
 //TODO seed observations
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('observations').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('observations').insert({
+          id: 1,
+          user_id: 1,
+          latitude: 43.0760,
+          longitude: 107.2903,
+          stars: 4,
+          name: 'Wyoming Star Spot',
+          description: 'This spot was awesome'
+        }),
+
+        knex('observations').insert({
+          id: 2,
+          user_id: 2,
+          latitude: 40.0150,
+          longitude: 105.2705,
+          stars: 2,
+          name: 'Galvanize Balcony',
+          description: 'Ok, Devin kept shining a flashlight at me'
+        }),
+
+        knex('observations').insert({
+          id: 3,
+          user_id: 3,
+          latitude: 39.7330,
+          longitude: 105.2391,
+          stars: 4,
+          name: 'Golden Lookout Mountain',
+          description: 'It was awesome. I love stars!'
+        })
       ]);
+    });
+    .then(function(){
+      return knex.raw("SELECT setval('observations_id_seq', (SELECT MAX(id) FROM observations))");
     });
 };
