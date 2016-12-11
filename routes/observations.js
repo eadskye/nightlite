d4887f54857699405f3252fe60c0f29f803b0feb
Brad Efting <bradefting@gmail.com>
2016-12-10 19:27:14 -0700

    post observation route posting and responding

--- routes/observations.js
+++ routes/observations.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const router = express.Router();
 // const ev = require('express-validation');
-// const validations = require('../validations/comments');
+// const validations = require('../validations/observations');
 const knex = require('../knex');
-const {decamelizeKeys, camelizeKeys} = require('humps');
+// const {decamelizeKeys, camelizeKeys} = require('humps');
 const bcrypt = require('bcrypt');
 
 router.get('/observations', (req, res, next) => {
@@ -40,10 +40,27 @@ router.get('/observations/:user_id', (req, res, next) => {
 
 });
 
-// router.post('/observations', ev(validations.post), (req, res, next) => {
-//
-// });
-//
+//TODO validaton code router.post('/observations', ev(validations.post), (req, res, next) => {
+//TODO add error handling of no lat, long, stars, etc
+
+router.post('/observations', (req, res, next) => {
+
+  knex('observations')
+    .insert({
+      user_id: req.body.user_id,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      stars: req.body.stars,
+      name: req.body.name
+    })
+    .then(
+      res.send('observation post created')
+    )
+    .catch((err) => {
+      next(err);
+    });
+});
+
 // router.patch('/observations', ev(validations.patch), (req, res, next) => {
 //
 // });
