33210fed2d2be2293ad001bd6e93f1394b24a5e9
Brad Efting <bradefting@gmail.com>
2016-12-11 11:30:35 -0700

    observation post tests for empty data and errors if empty

--- routes/observations.js
+++ routes/observations.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-// const ev = require('express-validation');
-// const validations = require('../validations/observations');
+const ev = require('express-validation');
+const validations = require('../validations/observations');
 const knex = require('../knex');
 // const {decamelizeKeys, camelizeKeys} = require('humps');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const boom = require('boom');
 
 router.get('/observations', (req, res, next) => {
  knex('observations')
@@ -41,20 +42,45 @@ router.get('/observations/:user_id', (req, res, next) => {
 });
 
 //TODO validaton code router.post('/observations', ev(validations.post), (req, res, next) => {
-//TODO add error handling of no lat, long, stars, etc
 
-router.post('/observations', (req, res, next) => {
+router.post('/observations', ev(validations.post), (req, res, next) => {
+
+  const newObservation = {
+    user_id: req.body.user_id,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    stars: req.body.stars,
+    name: req.body.name,
+    description: req.body.description
+   };
+
+  if(!newObservation.user_id){
+    return next(boom.create(400, 'user_id must not be blank'));
+  }
+  if (!newObservation.latitude) {
+    return next(boom.create(400, 'latitude must not be blank'));
+  }
+
+  if (!newObservation.longitude) {
+    return next(boom.create(400, 'longitude must not be blank'));
+  }
+
+  if (!newObservation.stars) {
+    return next(boom.create(400, 'stars must not be blank'));
+  }
+
+  if (!newObservation.name) {
+    return next(boom.create(400, 'name must not be blank'));
+  }
+
+  if (!newObservation.description) {
+    return next(boom.create(400, 'description must not be blank'));
+  }
 
   knex('observations')
-    .insert({
-      user_id: req.body.user_id,
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
-      stars: req.body.stars,
-      name: req.body.name
-    })
+    .insert(newObservation)
     .then(
-      res.send('observation post created')
+      res.send('New observation post created')
     )
     .catch((err) => {
       next(err);
@@ -65,7 +91,34 @@ router.post('/observations', (req, res, next) => {
 //
 // });
 //
-// router.delete('/observations', ev(validations.delete), (req, res, next) => {
+// router.delete('/observations', (req, res, next) => {
+//   console.log("here");
+//     const deleteId = Number.parseInt(req.params.id);
+//     // console.log(deleteId);
+//     // if(isNaN(deleteId) || deleteId<0){
+//     //   res.sendStatus(404);
+//     // }
+//
+//     knex('observations')
+//       .where('id', deleteId)
+//       .first()
+//       .then((deleteObs) =>{
+//         if(!deleteObs){
+//           return next();
+//         }
+//         console.log(deleteObs);
+//         return knex('observations')
+//           .del()
+//           .where('id', deleteId);
+//       })
+//
+//       .then(() => {
+//         delete deleteObs.id;
+//         res.send("its gone");
+//       })
+//       .catch((err) => {
+//         next(err);
+//       });
 //
 // });
 
