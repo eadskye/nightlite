1911081326301349849fb33ffed89e06c257df1e
Kyle Joyce <kyyjoyce@gmail.com>
2016-12-11 20:22:16 -0700

    added db connection in knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,8 @@ module.exports = {
     connection:'postgres://localhost/nightlite_test'
   },
 
-  production: {}
+  production: {
+  	  client: 'pg',
+  	  connection: process.env.DATABASE_URL
+  	}
 };
