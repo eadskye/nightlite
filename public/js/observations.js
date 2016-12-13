24385745b30dcd37b925011eb78b73b3a131b609
Anna Mefford <annamefford@gmail.com>
2016-12-12 20:39:10 -0700

    working buttons need formatting

--- public/js/observations.js
+++ public/js/observations.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
   getObservations();
-  
+
 
 function getObservations(data){
 $.ajax({
--- public/js/updatecomment.js
+++ public/js/updatecomment.js
@@ -32,18 +32,18 @@ function getComments(data){
 function drawComments(data){
   var i;
   var results = [];
-
+  $('#comments').empty();
   for (i=0 ; i<data.length ; i++){
     results.push(
-        '<div class="card-panel white">' +
-            '<span class="black-text"> "' + data[i].comment +
-            '"</span>' +
-            '<div class="black-text"> Posted By:' + data[i].username + '</div>' +
-            '<div class="black-text"> Star Rating: ' + data[i].stars + '</div>' +
-            '<div class="black-text"> PostedDate: ' + data[i].created_at + '</div>' +
-        '</div>' +
-      '<div class="col s2 "></div>' +
-    '</div>');
+      '<div class="card-panel white">' +
+        '<span class="black-text"> "' + data[i].comment +
+        '"</span>' +
+        '<div class="black-text"> Posted By: ' + data[i].username + '</div>' +
+        '<div class="black-text"> Star Rating: ' + data[i].stars + '</div>' +
+        '<div class="black-text"> PostedDate: ' + data[i].created_at + '</div>' +
+        '<a id="updatepost' + i + '" class="btn waves-effect waves-light blue lighten-2">Update</a>' +
+        '<a id="deletepost" class="btn waves-effect waves-light blue lighten-2">Delete</a>' +
+      '</div>');
   }
   $('#comments').append(results.join(''));
 }
