e03d6949aae0c21bccc664de99e49e338015f222
Kyle Joyce <kyyjoyce@gmail.com>
2016-12-13 20:33:02 -0700

    targeting new comment, preparing to post

--- public/js/observations.js
+++ public/js/observations.js
@@ -5,29 +5,25 @@ $(document).ready(function() {
     getObservations();
 
     $(".information").on('click', function(event) {
-        console.log(event.target);
+        // console.log(event.target);
+
         if ($(event.target).hasClass('comments-button')) {
-            console.log(event.target.getAttribute('attr'));
+
             obsID = event.target.getAttribute('attr');
-
+            console.log(obsID);
             getComments();
-
             $('#modal1').modal('open');
         }
         if ($(event.target).hasClass('submit-button')) {
-            console.log('submit');
+            obsID = event.target.getAttribute('attr');
+            console.log(obsID);
+            var containerDiv = event.target.closest('div');
+            var input = $(containerDiv).find('input');
+            var newComment = $(input).val();
+            $(input).val('');
+            addComment(newComment);
         }
     });
-
-    $(".submit-button").on('click', function(event) {
-        event.preventDefault();
-        console.log('submit clicked');
-    });
-
-    $(".comments-button").on('click', function(event) {
-        console.log('submit clicked');
-    });
-
 });
 
 var obsData;
@@ -50,6 +46,23 @@ function getObservations(data) {
     });
 }
 
+function addComment() {
+  $.ajax({
+      // url: 'https://nightlited.herokuapp.com/observations',
+      url: 'http://localhost:8000/observations/',
+      jsonp: "callback",
+      data: data,
+      type: 'get',
+      success: function(data) {
+          obsData = data;
+          drawObservations(obsData);
+      },
+      error: function() {
+          console.log("error");
+      }
+  });
+}
+
 function getComments(data) {
     console.log("in HERE");
     $.ajax({
@@ -64,7 +77,8 @@ function getComments(data) {
             $('#content').empty();
 
             // displays no comments if there are none to display
-            if (data.length === 0) {
+            // updated to handle for undefined data
+            if (typeof data === typeof undefined || data.length === 0) {
                 $('#content').append(
                     '<h5 class="black-text">No comments to display.</h5>'
                 );
@@ -106,19 +120,19 @@ function drawObservations(data) {
         template.push(
             '<div class="row">' +
             '<div class="col s12">' +
-            '<h5 class="black-text">' + data[i].name + '</h5>' +
-            '<div class="black-text">Location:  ' + data[i].name + '</div>' +
-            '<div class="black-text">Description:  ' + data[i].description + '</div>' +
-            '<div class="black-text">Rating:  ' + data[i].stars + '</div>' +
-            '<div class="black-text">Coordinates:  Latitude:  ' + data[i].latitude + '  Longitude:  ' + data[i].longitude + '</div>' +
-            '<div class="black-text">Date:  ' + date + '</div>' +
+            '<h5 class="bold black-text">' + data[i].name + '</h5>' +
+            '<div class="bold black-text">Location:  ' + data[i].name + '</div>' +
+            '<div class="bold black-text">Description:  ' + data[i].description + '</div>' +
+            '<div class="bold black-text">Rating:  ' + data[i].stars + '</div>' +
+            '<div class="bold black-text">Coordinates:  Latitude:  ' + data[i].latitude + '  Longitude:  ' + data[i].longitude + '</div>' +
+            '<div class="bold black-text">Date:  ' + date + '</div>' +
             '<div class="black-text">Posted by:  ' + data[i].username + '</div>' +
             // '<div class="id black-text">ID: ' + data[i].id + '</div>' +
 
             '<div class="input-field s6 m6 l3">' +
             '<input placeholder="Add a comment" type="text" class="validate comments">' +
             '<label class="active black-text" for="comments">Comments: ' + '</label>' +
-            '<button class="submit-button white-text col s3 btn waves-effect waves-light blue lighten-1" type="button" name="make-comment">Add Comment</button>' +
+            '<button attr=' + data[i].id + ' class="submit-button white-text col s3 btn waves-effect waves-light blue lighten-1" type="button" name="make-comment">Add Comment</button>' +
             '<button attr=' + data[i].id + ' class="comments-button white-text col s3 btn waves-effect waves-light blue lighten-1" type="button" name="view-comment">View Comments</button>' +
             '<div id="commentsDiv' + data[i].id + '">' +
             '</div>' +
