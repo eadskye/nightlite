6f215c5df6360b42e7933193a646c6f712d509e1
Anna Mefford <annamefford@gmail.com>
2016-12-14 18:04:57 -0700

    update url on login

--- public/js/updatecomment.js
+++ public/js/updatecomment.js
@@ -67,7 +67,6 @@ $(document).ready(function() {
     }
 
     function updateComment(id, text) {
-<<<<<<< HEAD
         let commentid = id;
         let newcomment = text;
         var data = {
@@ -94,20 +93,7 @@ $(document).ready(function() {
             .fail(($xhr) => {
                 console.log('shr response text');
             });
-=======
-      let commentid = id;
-      let newcomment = text;
-      var data = {
-        comment: newcomment
-      }
-      const options = {
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        dataType: 'json',
-        type: 'PATCH',
-        url: 'http://localhost:8000/comments/' + commentid
-      };
->>>>>>> 31a95b63d0b99aa84f1749dfd91d40e20ce33252
+
 
       $.ajax(options)
         .done(() => {
